acd3325c7c460c58c3a42ff05aeab13807b51a82
sunnykumar70 <sunnyroy150353@gmail.com>
2024-06-24 23:17:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Here you would typically send the username and password to a server for authentication
+    // For simplicity, let's just log them for now
+    console.log("Username: " + username + ", Password: " + password);
+}
